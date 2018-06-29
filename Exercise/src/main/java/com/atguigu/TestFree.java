45f9c6defe851f0d5bcd60afa3cd2966df39256d
MrMiMeType <zhangjianchun8023@163.com>
2018-06-30 00:05:38 +0800

    来自MrMiMeType的第三次提交

--- Exercise/src/main/java/com/atguigu/TestFree.java
+++ Exercise/src/main/java/com/atguigu/TestFree.java
@@ -5,6 +5,7 @@ public class TestFree {
 	public static void main(String[] args) {
 		System.out.println("这是MrMiMeType的第一次提交！");
 		System.out.println("这是MrMiMeType的第二次提交！");
+		System.out.println("这是MrMiMeType的第三次提交！");
 	}
 
 }
